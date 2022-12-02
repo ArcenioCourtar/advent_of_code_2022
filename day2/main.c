eb6141b95a8460c2ee79f7f4b8e10af6e410fc96
Arcenio <71286201+ArcenioCourtar@users.noreply.github.com>
2022-12-02 16:57:15 +0100

    Day2, Part2 done

--- day2/main.c
+++ day2/main.c
@@ -3,35 +3,62 @@
 #include	<stdio.h> // printf()
 #define		BUFFER_SIZE 15000
 
-int	compare_inputs(char elf, char you)
+// A = rock, B = paper, C = scissors
+int	compare_inputs_p1(char elf, char you)
 {
 	int	score = 0;
 
-	you -= 23;
-	if (you == 'A')
-		score += 1;
-	else if (you == 'B')
-		score += 2;
-	else if (you == 'C')
-		score += 3;
+	you -= 23; // Convert XYZ column to ABC for clarity
+	score += you - 64; // I love ASCII values (value of 'A' is 65)
 
-	if (elf == you)
+	if (elf == you) // draw
 		score += 3;
-	else if ((elf == 'A' && you == 'C') || (elf == 'B' && you == 'A') || (elf == 'C' && you == 'B'))
+	else if ((elf == 'A' && you == 'C') || (elf == 'B' && you == 'A') || (elf == 'C' && you == 'B')) // loss
 		score += 0;
-	else
+	else // win
 		score += 6;
 	return (score);
 }
 
-int	calculate_score(char *buf)
+// X = loss, Y = draw, Z = win
+int compare_inputs_p2(char elf, char result)
+{
+	int	score = 0;
+
+	if (result == 'Y') // draw
+		score += elf - 64 + 3;
+	else if (result == 'X') // loss
+	{
+		if (elf == 'A')
+			score += 3;
+		if (elf == 'B')
+			score += 1;
+		if (elf == 'C')
+			score += 2;
+	}
+	else if (result == 'Z') // winner
+	{
+		if (elf == 'A')
+			score += 8;
+		if (elf == 'B')
+			score += 9;
+		if (elf == 'C')
+			score += 7;
+	}
+	return (score);
+}
+
+int	calculate_score(char *buf, int part)
 {
 	int	i = 0;
 	int total = 0;
 
 	while (buf[i] != '\0')
 	{
-		total += compare_inputs(buf[i], buf[i+2]);
+		if (part == 1)
+			total += compare_inputs_p1(buf[i], buf[i+2]);
+		if (part == 2)
+			total += compare_inputs_p2(buf[i], buf[i+2]);
 		i += 4;
 	}
 	return (total);
@@ -45,6 +72,8 @@ int	main(void)
 	int fd = open("input.txt", O_RDONLY);
 	read(fd, buf, BUFFER_SIZE);
 
-	answer = calculate_score(buf);
+	answer = calculate_score(buf, 1);
+	printf("%i\n", answer);
+	answer = calculate_score(buf, 2);
 	printf("%i\n", answer);
 }
